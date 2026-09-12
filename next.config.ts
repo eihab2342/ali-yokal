f805e173b9058ee7332048e2910fb0e1c7be4ff1
Eihab <eihab2342@gmail.com>
2026-09-12 12:30:48 +0300

    fix next.config images

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,11 @@ const nextConfig: NextConfig = {
         pathname: "/**",
       },
       {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+      },
+      {
         protocol: 'http',
         hostname: '127.0.0.1',
         port: '8000',
